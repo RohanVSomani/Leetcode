b45bb9f456e37b2735d0ab2ba6d1d7f8cbdf6ced
Rohan V Somani <rohanvsomani@gmail.com>
2026-05-21 19:19:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.80%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n<0)
+        x=1/x;
+        double pow = 1;
+        while(n!=0){
+            if((n%2)!=0)
+            pow*=x;
+            x*=x;
+            n/=2;
+        }
+        return pow;
+
+    }
+};

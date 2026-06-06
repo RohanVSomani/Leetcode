97768a2dca88f2e064beb738c4f43b67fdc34c49
Rohan V Somani <rohanvsomani@gmail.com>
2026-06-06 20:48:40 +0530

    Time: 545 ms (80.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,7 @@
+with cte as (
+    select num,
+    lead(num,1) over() as num1,
+    lead(num,2) over() as num2
+    from logs
+)
+select distinct num as ConsecutiveNums from cte where num=num1 and num1=num2

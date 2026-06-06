c2609184bc8d9a777736aad4f8bb1eb8527b1a49
Rohan V Somani <rohanvsomani@gmail.com>
2026-06-06 20:55:55 +0530

    Time: 373 ms (76.04%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select distinct p1.email as Email from Person P1,Person P2 where p1.email = p2.email and p1.id !=p2.id
+#select distinct p1.email as Email from Person P1,Person P2 where p1.email = p2.email and p1.id !=p2.id
+select email as Email from Person group by email having count(*)>1
